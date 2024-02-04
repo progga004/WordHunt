150ce406d7ce3af9c0c1412949a36366d0718804
Jbarkume <jamieson.barkume@stonybrook.edu>
2024-02-04 13:22:09 -0500

    added login logic with cookies

--- server/routers/users-router.js
+++ server/routers/users-router.js
@@ -3,7 +3,7 @@ const userRouter= express.Router()
 const mod = require('../models');
 const User =mod["User"]
 
-
+// returns all user records
 userRouter.get("/", async (req, res) => {
     try {
         const user = await User.find();
@@ -12,6 +12,8 @@ userRouter.get("/", async (req, res) => {
         res.status(500).send(err);
       }
 });
+
+// finds user with id
 userRouter.get("/:id", async (req, res) => {
     try {
         let user = await User.findById(req.params.id);
@@ -21,23 +23,33 @@ userRouter.get("/:id", async (req, res) => {
       }
 });
 
-userRouter.get("/login", async (req,res)=>{
-    try {
-        let username = await req.params.username;
-        res.json(username);
-    }
-    catch (err) {
-    res.status(500).send(err);
-}
-})
 
+userRouter.get("/login", (req,res)=> {
+    // Check if the user has a cookie with a key of "username"
+    const existingUsername = req.cookies.username;
+    console.log(existingUsername);
+
+    if (existingUsername) {
+        // If the user has a cookie, assign the existing username to them
+        res.cookie('username', existingUsername);
+    } else {
+        // If the user does not have a cookie, generate a random username
+        const randomUsername = "hello There" // subject to change
+        
+        // Send a cookie with the random username
+        res.cookie('username', randomUsername);
+    }
+});
+
+
+// creates user
 userRouter.post("/", async (req, res) => {
     let user = new User({
         username: req.body.username,
-        game: req.body.game
+        games: req.body.games
     });
     try {
-        const newUser = await User.save();
+        const newUser = await user.save();
         res.status(201).json(newUser);
     } catch (err) {
         res.status(400).json({ message: err.message })

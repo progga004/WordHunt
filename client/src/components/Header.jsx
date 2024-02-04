4424027b37627bc4b7948eff46a191bebd90626e
Anten Manuuel <anten.manuuel@stonybrook.edu>
2024-02-04 12:12:56 -0500

    fix font size

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <div>
       <header className="flex justify-center items-center px-8 py-4 mx-auto absolute top-0 bg-black z-10 w-full">
-        <div className="text-white flex font-bold text-xl">
+        <div className="text-white flex font-bold text-3xl">
           Welcome to Word Hunt
         </div>
       </header>

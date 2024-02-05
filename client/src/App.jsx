665b921f2524524e4add42a355be5de6dffaec7b
IFore134 <ian.fore@stonybrook.edu>
2024-02-05 01:02:30 -0500

    stats page created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,6 @@ import './App.css'
 import Header from './components/Header'
 import HomePage from './pages/HomePage'
 import GamePage from './pages/GamePage'
-
-import HomePage from './pages/HomePage'
 import StatsPage from './pages/StatsPage'
 function App() {
   return (

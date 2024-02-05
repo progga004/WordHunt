97fe488479bab3cb9884ebe57c83eb9d32fa39ee
jamie <jamie@barkume.com>
2024-02-05 14:24:47 -0500

    fixed socket logic

--- server/socket.js
+++ server/socket.js
@@ -3,27 +3,36 @@ const socketIo = require('socket.io');
 function handleSocketConnection(server) {
     const io = socketIo(server);
 
-    const rooms = new Map();
+    const rooms = [0]; // array of rooms where each element is how many people are in the room
+    let lastRoom = 0 // keeps track of last room populated so no users are left waiting indefinitely
 
     io.on('connection', socket => {
 
         // find and join room
-        let room = findRoom();
-        socket.join(room);
-
-        if (rooms.get(room) === 2)
-            // broadcast to room that players have been found
-            io.to(room).emit("PLAYER FOUND")
-
+        let idx;
+        if (rooms[lastRoom] == 1)
+            idx = lastRoom
         else
-            // broadcast to room (single player) to wait
-            io.to(room).emit("FINDING PLAYER");
+            idx = findRoom();
+
+        // join room and update last index and room
+        lastRoom = idx;
+        rooms[idx]++;
+        let currRoom = `room ${idx}`
+        socket.join(currRoom);
+
+        // send to client that they should wait for another player
+        socket.currRoomemit("FINDING PLAYER");
 
 
         /**
-         * PUT THE REST OF THE SOCKET LOGIC BELOW HERER
+         * PUT THE REST OF THE SOCKET LOGIC BELOW HERE
          */
 
+        // check if two players found
+         if (rooms[idx] === 2)
+            // broadcast to room that players have been found
+            io.to(currRoom).emit("PLAYER FOUND");
         
 
 
@@ -32,24 +41,21 @@ function handleSocketConnection(server) {
         socket.on("disconnect", () => {
 
             // send game over to client and set people in room to 0
-            io.to(room).emit("GAME OVER");
-            rooms.set(room, 0);
+            io.to(currRoom).emit("GAME OVER");
+            rooms[idx] = 0;
         });
     });
 
     const findRoom = () => {
         // logic for finding first available room
-        for (let [room, numInRoom] of rooms) {
-            if (numInRoom < 2) {
-                rooms.set(room, numInRoom + 1);
-                return room;
-            }
+        for (let i = 0; i < rooms.length; i++) {
+            if (rooms[i] < 2)
+                return i;
         }
     
-        // appends new room with 1 as the occupying space
-        let room = `room${rooms.size}`
-        rooms.set(room, 1);
-        return room;
+        // appends new room
+        rooms.push(0);
+        return rooms.length - 1;
     }
 }
 

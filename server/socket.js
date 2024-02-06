524453e9fa2d0c54d1783c71b629d367f1566829
jamie <jamie@barkume.com>
2024-02-05 21:17:47 -0500

    sockets almost finished

--- server/socket.js
+++ server/socket.js
@@ -3,37 +3,75 @@ const socketIo = require('socket.io');
 function handleSocketConnection(server) {
     const io = socketIo(server);
 
-    const rooms = [0]; // array of rooms where each element is how many people are in the room
+    const rooms = [];
     let lastRoom = 0 // keeps track of last room populated so no users are left waiting indefinitely
 
     io.on('connection', socket => {
 
         // find and join room
-        let idx;
-        if (rooms[lastRoom] == 1)
-            idx = lastRoom
-        else
-            idx = findRoom();
+        let idx = rooms[lastRoom]["numUsers"] === 1 ? lastRoom : findRoom();
+        let username = "";
 
         // join room and update last index and room
         lastRoom = idx;
-        rooms[idx]++;
+        rooms[idx]["numUsers"]++;
+
         let currRoom = `room ${idx}`
         socket.join(currRoom);
 
-        // send to client that they should wait for another player
-        socket.currRoomemit("FINDING PLAYER");
+        // User sends username
+        socket.on("USERNAME", user => {
+            username = user;
+            if (rooms[idx]["player1"] === null)
+                rooms[idx]["player1"] = username;
+            else
+                rooms[idx]["player2"] = username;
+
+            // send to client that they should wait for another player
+            socket.emit("FINDING PLAYER");
+
+            // check if two players found
+            if (rooms[idx]["numUsers"] === 2)
+                // broadcast to room that players have been found
+                io.to(currRoom).emit("PLAYER FOUND");
+        });
 
 
         /**
          * PUT THE REST OF THE SOCKET LOGIC BELOW HERE
          */
-
-        // check if two players found
-         if (rooms[idx] === 2)
-            // broadcast to room that players have been found
-            io.to(currRoom).emit("PLAYER FOUND");
         
+        socket.on("WORD CHOICE", word => {
+
+            // store word
+            if (rooms[idx]["player1"] === username)
+                rooms[idx]["player1Word"] = word;
+            else
+                rooms[idx]["player2Word"] = word;
+
+            if (rooms[idx]["player1Word"] && rooms[idx]["player2Word"])
+                io.to(currRoom).emit("SWITCH TURN", rooms[idx]["player1"]);
+            else
+                socket.emit("WAITING FOR OTHER WORD CHOICE");
+        });
+
+        socket.on("WORD GUESS", word => {
+            let wordToCompare;
+
+            // find which user to compare against
+            if (rooms[idx]["player1"] === username)
+                wordToCompare = rooms[idx]["player2Word"]
+            else
+                wordToCompare = rooms[idx]["player1Word"];
+            
+            // compare letters and send info to clients
+            if (wordToCompare === word)
+                    io.to(currRoom).emit("GAME OVER", username)
+            else {
+                let lettersInCommon = findLetters(word, wordToCompare);
+                io.to(currRoom).emit("SWITCH TURN", wordToCompare, lettersInCommon);
+            }
+        })
 
 
         // ----------------------------------------------
@@ -42,7 +80,15 @@ function handleSocketConnection(server) {
 
             // send game over to client and set people in room to 0
             io.to(currRoom).emit("GAME OVER");
-            rooms[idx] = 0;
+
+            // clean up room
+            rooms[idx] = {
+                "player1": null,
+                "player2": null,
+                "player1Word": null,
+                "player2Word": null,
+                "numUsers": 0
+            };
         });
     });
 
@@ -54,8 +100,19 @@ function handleSocketConnection(server) {
         }
     
         // appends new room
-        rooms.push(0);
+        rooms.push({
+            "player1": null,
+            "player2": null,
+            "player1Word": null,
+            "player2Word": null,
+            "numUsers": 0
+        });
         return rooms.length - 1;
+    }
+
+    // have questions about this
+    const findLetters = (word1, word2) => {
+        let numInCommon = 0
     }
 }
 
